e39a095146d4010c5dc0bffd2920f2cc247b2868
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-03-13 22:45:25 -0400

    saving progress

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+#include "MazeGenerator.h"
+#include "Labyrinth.h"
+#include <iostream>
+#include <string>
+
+const std::string kYourName = "AnitaProva";
+
+/* Change these constants to contain the paths out of your mazes. */
+const std::string kPathOutOfNormalMaze = "E";
+const std::string kPathOutOfTwistyMaze = "";
+
+int main()
+{
+    MazeCell *startLocation = mazeFor(kYourName);
+
+    /* Set a breakpoint here to explore your maze! */
+    if (isPathToFreedom(startLocation, kPathOutOfNormalMaze))
+    {
+        std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
+    }
+    else
+    {
+        std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
+    }
+
+    MazeCell *twistyStartLocation = twistyMazeFor(kYourName);
+
+    /* Set a breakpoint here to explore your twisty maze! */
+
+    /*if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze)) {
+        std::cout << "Congratulations! You've found a way out of your twisty labyrinth." << std::endl;
+    } else {
+        std::cout << "Sorry, but you're still stuck in your twisty labyrinth." << std::endl;
+    }*/
+
+    return 0;
+}
